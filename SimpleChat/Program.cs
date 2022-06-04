c9c65b72095c0e415e54895a37dd100134605836
JAELIM AN <ajl66@naver.com>
2022-06-04 23:34:24 +0900

    dd

--- SimpleChat/Program.cs
+++ SimpleChat/Program.cs
@@ -1,6 +1,35 @@
+using SimpleChat.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.MapHub<ChatHub>("/chatHub");
+
+app.Run();
+
+/*
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet( "/", () => "Hello World!");
 
 app.Run();
+*/
